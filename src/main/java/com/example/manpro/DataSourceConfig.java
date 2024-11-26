638ba60bb122bf8a756164e5ab7f577b1956a260
felikssitorus <felikssitorus@gmail.com>
2024-11-26 12:50:31 +0700

    memperbaiki kode

--- src/main/java/com/example/manpro/DataSourceConfig.java
+++ src/main/java/com/example/manpro/DataSourceConfig.java
@@ -3,8 +3,10 @@ package com.example.manpro;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+@Configuration
 public class DataSourceConfig {
    @Value("${spring.datasource.url}")
    private String url;
@@ -12,9 +14,6 @@ public class DataSourceConfig {
    private String username;
    @Value("${spring.datasource.password}")
    private String password;
-
-   public DataSourceConfig() {
-   }
 
    @Bean
    public DataSource dataSource() {
